1624b6e7772b1b8a796a6eef10a53c1d3fb6883f
Declan <dketchum@olin.edu>
2021-06-22 16:42:23 -0400

    Tutorial Draft

--- Clew/Tutorial/TutorialTestViews.swift
+++ Clew/Tutorial/TutorialTestViews.swift
@@ -8,18 +8,51 @@
 
 import SwiftUI
 
+struct TutorialScreen<Content: View>: View {
+    @State private var score = 0
+  let content: Content
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+  var body: some View {
+    content
+    NavigationView{
+        if #available(iOS 14.0, *) {
+            Text("Score: \(score)")
+                .navigationTitle("CLEW Tutorial")
+                .navigationBarItems(
+                    leading:
+                        Button("Subtract 1") {
+                            self.score -= 1
+                                },
+                    trailing:
+                        Button("Add 1") {
+                            self.score += 1})
+        } else {
+            // Fallback on earlier versions
+        }
+        
+    /*.navigationBarTitle(Text(NSLocalizedString("surveyPopoverTitle", comment: "This is the title of the survey popover that is displayed to get feedback")), displayMode: .inline).navigationBarItems(trailing: Button(action: {
+        NotificationCenter.default.post(name: Notification.Name("SurveyPopoverReadyToDismiss"), object: nil)
+    }) { Text(NSLocalizedString("dismissSurvey", comment: "this is the button text that dismisses the survey"))}) */
+    }
+  }
+}
+
+
 struct TutorialTestView: View {
     var body: some View {
-        NavigationView{
-            VStack {
-                Text("CLEW Tutorial")
-                
-                NavigationLink(destination: OrientPhone()) {Text("Holding Your Phone")}
-                
-                NavigationLink(destination: SetAnchorPoint()) {Text("Setting an Anchor Point")}
-                
-                NavigationLink(destination: SignleUse()) {Text("Using a Signle Use Route")}
+        TutorialScreen{
+                VStack (spacing: 30){
+                    
+                    
+                    NavigationLink(destination: OrientPhone()) {Text("Holding Your Phone")}
+                    
+                    NavigationLink(destination: SetAnchorPoint()) {Text("Setting an Anchor Point")}
+                    
+                    NavigationLink(destination: SignleUse()) {Text("Using a Signle Use Route")}
 
+                
             }
         }
     }
@@ -27,7 +60,7 @@ struct TutorialTestView: View {
 
 struct OrientPhone: View {
     var body: some View {
-        NavigationView{
+        TutorialScreen {
             VStack{
                 Text("Holding Your Phone")
             
@@ -35,20 +68,20 @@ struct OrientPhone: View {
                 
                 NavigationLink(destination: SetAnchorPoint()) {Text("Next")}
             }
+            
         }
     }
 }
 
 struct SetAnchorPoint: View {
     var body: some View {
-        NavigationView{
             VStack{
                 Text("Setting an Anchor Point")
             
                 Text("To allow your route to be navigated at a later pointyou need to record an anchor point for the start of your route...")
             
                 NavigationLink(destination: SignleUse())  {Text("Next")}
-            }
+            
         }
     }
 }
@@ -62,6 +95,27 @@ struct SignleUse: View {
         }
     }
 }
+
+struct FindingSavedRoutes: View {
+    var body: some View {
+        VStack{
+            Text("Finding Saved Routes")
+            
+            Text("")
+        }
+    }
+}
+
+struct SettingOptions: View {
+    var body: some View {
+        VStack{
+            Text("Settings Options")
+            
+            Text("")
+        }
+    }
+}
+
 
 
 
